cb4cf9addef04bfa105c83a5afee1bfec4399c52
MinsuKim21 <minsu020302@gmail.com>
2022-01-30 17:20:49 +0900

    version3 is not working

---
+++ vue3-login/src/main.js
@@ -0,0 +1,14 @@
+// import Vue from 'vue'
+import { createApp } from 'vue'
+import App from './App.vue'
+// import {store} from './store/store'
+
+// new Vue({
+//     el: '#app',
+//     store,
+//     render: h => h(App)
+// })
+
+createApp(App).mount('#app')
+
+
